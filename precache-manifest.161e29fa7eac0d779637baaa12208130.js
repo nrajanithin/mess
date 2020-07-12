5ec7610f6577e60ff217d3a66450c591508894d2
nrajanithin <nrajanithin@gmail.com>
2020-07-12 18:22:19 +0530

    Updates

---
+++ precache-manifest.161e29fa7eac0d779637baaa12208130.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ff5af5ce41c3026c88e5d4cb824190",
+    "url": "/mess/index.html"
+  },
+  {
+    "revision": "04d55616ca5eb426d764",
+    "url": "/mess/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2eb7c9220e24a3401bff",
+    "url": "/mess/static/js/2.eff3cbde.chunk.js"
+  },
+  {
+    "revision": "2954b1568600ec1d7b70d079ce9ac1a4",
+    "url": "/mess/static/js/2.eff3cbde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d55616ca5eb426d764",
+    "url": "/mess/static/js/main.36feab04.chunk.js"
+  },
+  {
+    "revision": "3b949f91ef2f8f252f17",
+    "url": "/mess/static/js/runtime-main.647b4a4f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mess/static/media/logo.5d5d9eef.svg"
+  }
+]);
